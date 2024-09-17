4548bde43e887c5f1981bd58ab65d9fc953fab70
Erito <ero62792@gmail.com>
2024-09-17 15:31:20 -0400

    Cracion de componetes

---
+++ src/app/managements/asignments/asignments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asignments',
+  standalone: true,
+  imports: [],
+  templateUrl: './asignments.component.html',
+  styleUrl: './asignments.component.css'
+})
+export class AsignmentsComponent {
+
+}
